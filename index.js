0eee5696919247ae9f91194436712222a54285f6
Pimentel <Jeremiah.Pimentel@megaxcess.com>
2021-06-10 11:38:41 +0800

    inital REST API

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      app = express(),
+      mysql = require('mysql');
+
+app.use(bodyParser.json());
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'restful_db'
+});
+
+conn.connect((err) => {
+    if(err) throw err;
+    console.log('MySQL Connected...');
+});
+
+app.get('/api/httpRes/:id', (req, res) => {
+    let sql = "SELECT * FROM products";
+    let query = conn.query(sql, (err, result) => {
+        if(err) throw err;
+        res.send(JSON.stringify({ "status": 200, "error": null, "response": result }));
+    })
+});
